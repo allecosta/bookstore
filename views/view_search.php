37496cd5d094a0540d8e54e3219af205f6caa7e9
allecosta <alle_costa10@outlook.com>
2022-10-31 17:20:06 -0300

    feat: add search page

---
+++ views/view_search.php
@@ -0,0 +1,36 @@
+<?php require_once './controllers/search.php'; ?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<?php require_once './templates/head.php'; ?>
+
+<body>
+
+<?php require 'header.php'; ?>
+
+    <main>
+        <section class="row container col-12">
+            <?php if ($products === false) : ?>
+                <h1>Nenhum produto encontrado!</h1>
+            <?php else : ?>
+                <?php foreach ($products as $product) : ?>
+                    <div class="css-container-product col-12 col-sm-6 col-md col-lg-3">
+                        <a href="product.php?id_product=<?php echo $product['id_product']; ?>">
+                            <div>
+                                <img src="image_product.php?id_product=<?php echo $product['id_product']; ?>" 
+                                alt="<?php echo $product['title']; ?>" style="max-width: 100%; height: 160px;">
+                                <h4><?php $product['title']; ?></h4>
+                                <p>R$ <?php echo formatPrice($product['price']); ?></p>
+                            </div>
+                        </a>
+                    </div>
+                <?php endforeach; ?>
+            <?php endif; ?>
+        </section>
+
+        <?php require './templates/pagination.php'; ?>
+    </main>
+    
+</body>
+</html>
