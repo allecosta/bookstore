d5d2bc4928f7ff41df0e0e27ab516be592461339
allecosta <alle_costa10@outlook.com>
2022-10-31 17:14:54 -0300

    feat: add user registered page

---
+++ views/user_registered.php
@@ -0,0 +1,21 @@
+<?php require_once '/lib/functions.php'; ?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<?php require_once './templates/head.php'; ?>
+
+<body>
+    
+<?php require 'header.php'; ?>
+
+    <main>
+        <section class="text-center">
+            <h1>Seu cadastro foi concluído com sucesso!</h1>
+            <div>
+                <a href="view_login.php">Ir para página de login</a>
+            </div>
+        </section> 
+    </main>
+</body>
+</html>
