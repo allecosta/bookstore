67eceed3d0fd5e77adb9ddae33fd3dc54e4e9f0b
allecosta <alle_costa10@outlook.com>
2022-11-07 17:52:53 -0300

    refactor: index page

--- views/index.php
+++ views/index.php
@@ -13,34 +13,27 @@
         <h1 id="welcome">Bem vindo à Book Store</h1>
         <section class="row container col-12">
             
-            <?php if ($products !== false) {
-                foreach ($$products as $key => $value) {
-                    $price = formatPrice($value['price']);
-                ?>
-                <div class="css-container-product col-12 col-sm-6 col-md-4 col-lg-3">
-                    <a href="view_product.php?id_product=<?php echo $value['id_product']; ?>">
-                        <div>
-                            <img src="image_product.php?id_product=<?php echo $value['id_product']; ?>" 
-                            alt="<?php echo $value['title']; ?>"
-                            style="max-width: 100%; height: 150px;"
-                            >
-                            <h4><?php echo $value['title']; ?></h4>
-                            <p>R$ <?php echo $price; ?></p>
-                        </div>     
-                    </a>
-                </div>
-            <?php
-
-                }
-            } 
-            
-            ?>
-
+            <?php if ($products !== false) : ?>
+                <?php foreach ($$products as $key => $value) : ?>
+                    <?php $price = formatPrice($value['price']); ?>
+                    <div class="css-container-product col-12 col-sm-6 col-md-4 col-lg-3">
+                        <a href="view_product.php?id_product=<?php echo $value['id_product']; ?>">
+                            <div>
+                                <img src="image_product.php?id_product=<?php echo $value['id_product']; ?>" 
+                                alt="<?php echo $value['title']; ?>"
+                                style="max-width: 100%; height: 150px;"
+                                >
+                                <h4><?php echo $value['title']; ?></h4>
+                                <p>R$ <?php echo $price; ?></p>
+                            </div>     
+                        </a>
+                    </div>
+                <?php endforeach ?>
+            <?php endif; ?>
         </section>
 
         <?php require '../templates/pagination.php'; ?>
-    </main>
-    
+    </main>  
 </body>
 </html>
 
