ba2383102360fa740f01144ce4f16fb5d6f75cd3
allecosta <alle_costa10@outlook.com>
2022-11-17 16:13:14 -0300

    feat: add navigation page

---
+++ account/navigation.php
@@ -0,0 +1,19 @@
+<style>
+    #account-nav li {
+        list-style-type: none;
+    }
+
+    #account-nav li a {
+        display: block;
+        padding: 0.1em 0;
+        border-bottom: 1px solid rgb(220, 220, 220);
+    }
+</style>
+<section id="account-nav" class="col-12 col-md-3 col-lg-2">
+    <ul>
+        <li><a href="?link=my_products">Produtos à venda</a></li>
+        <li><a href="view_account.php?link=buy">Produtos comprados</a></li>
+        <li><a href="?link=sold">Produtos vendidos</a></li>
+        <li><a href="?link=cart">Meu carrinho</a></li>
+    </ul>
+</section>
